9b476c80965f2affaaa87e8ff3d6202a74dc3995
Tom Porter <tomporter62@gmail.com>
2020-07-28 18:18:05 +0100

    Added websiteManager proj

---
+++ SharedLib/LoginTests/LoginNavigationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using WebsiteManager;
+
+namespace LoginTests
+{
+    public class LoginNavigationTests
+    {
+        private Website _website;
+        private const int _sleepTime = 5;
+
+        [SetUp]
+        public void Setup()
+        {
+            _website = new Website("firefox", _sleepTime, _sleepTime);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true);
+        }
+    }
+}
