b3ef8d99974b4e09e33e745d71d18a666146fe67
BrujoCervino_Laptop <project.verna@gmail.com>
2020-07-28 22:42:56 +0100

    Added: Configs, Results POM, Results Tests ✨📚
    ✨ Added a config reader which reads various things including the url, login details and more:
    
    (please get the config.json file -> add it to your project -> click 'properties' (F4) -> Copy To Output Directory: Copy if Newer)
    
    🌠Added ResultsPageObjectModel project
    
    ✨ Added a basic ResultsTests project

---
+++ SharedLib/SharedLib/BrowserConfigReader.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedLib
+{
+    // Exists for people whose browsers are not in the default location
+    public class BrowserConfigReader : ConfigReader
+    {
+        public static readonly string FirefoxLocation = configObj.browserLocations.Firefox;
+    }
+}
