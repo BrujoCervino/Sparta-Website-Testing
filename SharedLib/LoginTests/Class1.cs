a21f34cf2943f46044f211afae8726e3054c8f83
Tom Porter <tomporter62@gmail.com>
2020-07-28 17:35:57 +0100

    Added Login Tests projects

---
+++ SharedLib/LoginTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginTests
+{
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true);
+        }
+    }
+}
