f9b6d1b996a7339fff7e338f2ffd3bbc0ac16e8f
BrujoCervino_Laptop <project.verna@gmail.com>
2020-07-28 17:32:42 +0100

    Added basic framework for results page

---
+++ SharedLib/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+        
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
