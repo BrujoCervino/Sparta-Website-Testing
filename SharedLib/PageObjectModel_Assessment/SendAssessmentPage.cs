0026299b06aaaef7c7845af51707c5b026c5ca4a
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-28 19:31:01 +0100

    AssessmentPageElemets

--- SharedLib/PageObjectModel_Assessment/SendAssessmentPage.cs
+++ SharedLib/PageObjectModel_Assessment/SendAssessmentPage.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using OpenQA.Selenium;
 using SharedLib;
 
 namespace PageObjectModel_Assessment
 {
     public class SendAssessmentPage : SuperPage
     {
-        public SendAssessmentPage(IwebDriver seleniumDriver) : base(seleniumDriver)
+        public SendAssessmentPage(IWebDriver seleniumDriver) : base(seleniumDriver)
         {
             _url = "https://uat.spartaglobal.academy/";
         }
