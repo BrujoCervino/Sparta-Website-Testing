7f31f76c37cb7ef5bc1769fff562a62c953586a6
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-28 17:37:05 +0100

    Created a Project for Assessment and installed packages

---
+++ SharedLib/PageObjectModel_Assessment/SendAssessmentPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedLib;
+
+namespace PageObjectModel_Assessment
+{
+    public class SendAssessmentPage : SuperPage
+    {
+        public SendAssessmentPage(IwebDriver seleniumDriver) : base(seleniumDriver)
+        {
+            _url = "https://uat.spartaglobal.academy/";
+        }
+
+    }
+}
