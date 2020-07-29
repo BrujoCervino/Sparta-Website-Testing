2efe3c9d87ac0d278812e1473c5a73eadd3194bc
Xenvastro <masimba36@outlook.com>
2020-07-29 16:10:14 +0100

    NG58FNLPRJ-54-Create_base_structure
    - Added Dispatches base
    - Added model to provide a key value list

---
+++ SharedLib/DispatchesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DispatchesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DispatchesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8102497d-5ef9-4f0a-8d1c-991ac9fa5325")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
