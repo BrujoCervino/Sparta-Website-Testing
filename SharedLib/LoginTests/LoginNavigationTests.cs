964753ebbd0ac546361e3d8959b4658fa9a980a4
Tom Porter <tomporter62@gmail.com>
2020-07-31 12:59:56 +0100

    Updated tests for chrome and firefox drivers

--- SharedLib/LoginTests/LoginNavigationTests.cs
+++ SharedLib/LoginTests/LoginNavigationTests.cs
@@ -8,36 +8,27 @@ namespace LoginTests
         private SpartaWebsite _chromeWebsite, _firefoxWebsite;
         private const int _sleepTime = 5;
 
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void ChromeDirver_WorkingCorrectly_Test()
         {
-            _chromeWebsite = new SpartaWebsite("chrome", _sleepTime, _sleepTime);
+            _chromeWebsite = new SpartaWebsite("chrome");
 
             _chromeWebsite.loginPage.Visit();
             Assert.That(_chromeWebsite.GetUrl(), Is.EqualTo("https://uat.spartaglobal.academy/"));
+
+            _chromeWebsite.Close();
         }
 
         [Test]
+        [Ignore("Firefox borwsers are not needed to be supported yet")]
         public void FirefoxDirver_WorkingCorrectly_Test()
         {
             _firefoxWebsite = new SpartaWebsite("firefox", _sleepTime, _sleepTime);
 
             _firefoxWebsite.loginPage.Visit();
             Assert.That(_firefoxWebsite.GetUrl(), Is.EqualTo("https://uat.spartaglobal.academy/"));
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            if (_chromeWebsite != null)
-                _chromeWebsite.Close();
-            if (_firefoxWebsite != null)
-                _firefoxWebsite.Close();
+            
+            _firefoxWebsite.Close();
         }
     }
 }
