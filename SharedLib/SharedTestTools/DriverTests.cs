1ccd68127b72aa9e893e12ad58b87978c7c9e742
Tom Porter <tomporter62@gmail.com>
2020-08-02 17:00:55 +0100

    Put driver tests in shared test tools proj

--- SharedLib/SharedTestTools/DriverTests.cs
+++ SharedLib/SharedTestTools/DriverTests.cs
@@ -11,11 +11,16 @@ namespace LoginTests
         [Test]
         public void ChromeDirver_WorkingCorrectly_Test()
         {
+            //Arange
             _chromeWebsite = new SpartaWebsite("chrome");
 
+            //Act
             _chromeWebsite.loginPage.Visit();
+
+            //Assert
             Assert.That(_chromeWebsite.GetUrl(), Is.EqualTo("https://uat.spartaglobal.academy/"));
 
+            //Teardown
             _chromeWebsite.Close();
         }
 
@@ -23,11 +28,16 @@ namespace LoginTests
         [Ignore("Firefox borwsers are not needed to be supported yet")]
         public void FirefoxDirver_WorkingCorrectly_Test()
         {
+            //Arange
             _firefoxWebsite = new SpartaWebsite("firefox", _sleepTime, _sleepTime);
 
+            //Act
             _firefoxWebsite.loginPage.Visit();
+
+            //Assert
             Assert.That(_firefoxWebsite.GetUrl(), Is.EqualTo("https://uat.spartaglobal.academy/"));
-            
+
+            //Teardown
             _firefoxWebsite.Close();
         }
     }
