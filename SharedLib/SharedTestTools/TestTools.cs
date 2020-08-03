387ec202da93f184b036feaaf0b2752034565020
Tom Porter <tomporter62@gmail.com>
2020-08-03 10:33:40 +0100

    Added login method to the shared test tools

--- SharedLib/SharedTestTools/TestTools.cs
+++ SharedLib/SharedTestTools/TestTools.cs
@@ -1,7 +1,17 @@
-﻿namespace SharedTestTools
+﻿using PageObjectModels;
+
+namespace SharedTestTools
 {
     public class TestTools
     {
         public static string GenerateWhiteSpaceString(int length) => new string(' ', length);
+
+        public static void Login(SpartaWebsite website)
+        {
+            website.loginPage.Visit();
+            website.loginPage.EnterUsername(LoginConfigReader.Username);
+            website.loginPage.EnterPassword(LoginConfigReader.Password);
+            website.loginPage.SubmitLoginInfo();
+        }
     }
 }
