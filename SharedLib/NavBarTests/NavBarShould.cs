f71aaeca89223009fa4becadd944ad499208820b
BrujoCervino_Laptop <project.verna@gmail.com>
2020-08-03 10:28:34 +0100

    Added NavBar tests

---
+++ SharedLib/NavBarTests/NavBarShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjectModels;
+
+namespace DispatchesTests
+{
+    [TestFixture]
+    public class NavBarShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        
+        [TestCase("Results", "Polls")]
+        public void GivenIAmOnThe_X_Page_TheSpartaLogoShould_SendMeToThe_Y_Page(in string initialPage, in string destinationPage)
+        {
+            SpartaWebsite sw = new SpartaWebsite("Chrome");
+            sw.SeleniumDriver.Navigate().GoToUrl(PagesConfigReader.BaseUrl + initialPage.ToLower());
+
+            IWebElement thing = sw.SeleniumDriver.FindElement(By.LinkText(destinationPage));
+            thing.Click();
+
+
+            Assert.That(sw.SeleniumDriver.Url, Is.EqualTo(PagesConfigReader.PollsUrl));
+        }
+    }
+}
---
+++ SharedLib/NavBarTests/NavBarWorks.feature
@@ -0,0 +1,11 @@
+﻿Feature: NavBarWorks
+	In order to be able to access other features of the website
+	As a user
+	I want to click a link in the navbar and be sent to the corresponding page
+
+@NavBar
+Scenario: Sparta
+	Given I am on the (.*) browser
+	And I am on the (.*) page
+	When I click the (.*) button in the navbar
+	Then I should be sent to the (.*) page
