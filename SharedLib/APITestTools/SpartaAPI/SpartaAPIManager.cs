7921b8d4651e50b5da1e80d282b04a4a7a659fbe
William Millington <WMillington@SpartaGlobal.com>
2020-08-03 17:24:39 +0100

    Delete Dispatches Added.

---
+++ SharedLib/APITestTools/SpartaAPI/SpartaAPIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using PageObjectModels;
+using RestSharp;
+
+namespace APITestTools.SpartaAPI
+{
+    public class SpartaAPIManager
+    {
+        private readonly IRestClient client;
+        public SpartaAPIManager()
+        {
+            client = new RestClient(PagesConfigReader.BaseUrl);
+        }
+        public string AuthorisationToDelete()
+        {
+            var request = new RestRequest("api/users/auth");
+            request.AddJsonBody(new { username = LoginConfigReader.Username, password = LoginConfigReader.Password });
+            var response = client.Execute(request, Method.POST);
+            return response.Content;
+        }
+        public string DeleteDispatchesTable(string token)
+        {
+            var request = new RestRequest("api/dispatch");
+            request.AddHeader("x-access-token", token);
+            var response = client.Execute(request, Method.DELETE);
+            return response.Content;
+        }
+    }
+}
---
+++ SharedLib/APITestTools/SpartaAPI/SpartaAPIService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using PageObjectModels;
+using RestSharp;
+
+namespace APITestTools.SpartaAPI
+{
+    public class SpartaAPIService
+    {
+        public SpartaAPIManager spartaManager = new SpartaAPIManager();
+        public string spartaAuthData;
+        public string spartaDeleteData;
+        public JObject json_auth_sparta;
+        public JObject json_delete_sparta;
+        private string token;
+
+        public SpartaAPIService()
+        {
+            spartaAuthData = spartaManager.AuthorisationToDelete();
+            json_auth_sparta = JsonConvert.DeserializeObject<JObject>(spartaAuthData);
+            token = json_auth_sparta["token"].ToString();
+        }
+        public void DeleteDispatches()
+        { 
+            spartaDeleteData = spartaManager.DeleteDispatchesTable(token);
+            json_delete_sparta = JsonConvert.DeserializeObject<JObject>(spartaDeleteData);
+        }
+    }
+    internal class test
+    {
+        [Test]
+        public void testing123()
+        {
+            SpartaAPIService sparta = new SpartaAPIService();
+            sparta.DeleteDispatches();
+            Assert.That(sparta.json_delete_sparta["success"].ToString(), Is.EqualTo("True"));
+        }
+    }
+}
